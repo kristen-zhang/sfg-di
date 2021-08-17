2e282be28cb466c205c49de625d415f835c81088
kristenzhang <kzhang@bellese.io>
2021-08-17 11:16:25 -0400

    initial

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -1,9 +1,11 @@
 package guru.springframework.sfgdi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
     public String sayHello() {
         System.out.println("hello world!!!");
         return "Hi folks!";
--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
+
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
